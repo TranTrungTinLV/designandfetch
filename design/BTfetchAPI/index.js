e9d527ae76ce5444142b8c8f136818fa55992692
TranTrungTinLV <trungtintin1989@gmail.com>
2022-06-25 21:35:43 +0700

    xongchucnangaddvadagetAPIveduoc
    đang nghiên cứu lại phần xóa và sửa

--- design/BTfetchAPI/index.js
+++ design/BTfetchAPI/index.js
@@ -1,34 +1,87 @@
 //renderTodo 
-var API = './API.json'
+var API = 'http://localhost:3000/lists'
 
 function start() {
-    for(var i = 0; i <= 100; i++){
-        gettodo(renderTodo);
-    }
+    gettodo(renderTodo);
+    handleCreateForm();
 }
+
 start();
+
 
 function gettodo(callback) {
 
-        fetch(API)
+    fetch(API)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(callback);
+}
+
+function Addtasks(data, callback) {
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+    };
+    fetch(API, options)
+        .then(function (response) {
+            response.json();
+        })
+        .then(callback);
+}
+
+function Delete(id) {
+    var Delete = document.querySelector('#btnDelete')
+    Delete.addEventListener('click', function () {
+        var options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        };
+        fetch(API + '/' + user.id + '/' + id, options)
             .then(function (response) {
-                return response.json();
+                response.json();
             })
             .then(callback);
+    })
 }
 
 function renderTodo(tasks) {
-        var listtodo = document.querySelector('#listtodo');
-        var htmls = tasks.map(function (task) {
+    var listtodo = document.querySelector('#listtodo');
+    var htmls = tasks.map(function (task) {
 
-            return `
-            <li>
-                <span>${task.id} :</span>
-                <span style = "color:green">${task.title}</span>
-                <span>${task.completed}</span>
-                </li>
+        return `
+               <table>
+               <td>${task.id}</td>
+               <td>${task.title}</td>
+               <td>${task.completed}</td>
+               </table>
+                
         `
-        });
-        listtodo.innerHTML = htmls.join(' ');
-    
+    });
+    listtodo.innerHTML = htmls.join(' ');
+
+}
+
+function handleCreateForm() {
+    var Addtask = document.querySelector('#btnAdd')
+
+    Addtask.addEventListener('click', function () {
+        var title = document.querySelector('input[name = "title"]').value;
+        console.log(title)
+        var completed = document.querySelector('input[name = "completed"]').value;
+        console.log(completed)
+        var form = {
+            title: title,
+            completed: completed
+        };
+
+        Addtasks(form)
+    })
 }
